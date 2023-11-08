f4a7266473c1ba5dd46895042ab88eb806c39893
Sophie Nguyen <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-11-08 12:47:08 -0500

    comment

--- main.ts
+++ main.ts
@@ -11,19 +11,27 @@ const servoNumber1 = robotbit.Servos.S1
 // setup
 basic.showIcon(IconNames.Heart)
 
+// button A pressed
 input.onButtonPressed(Button.A, function () {
+  // turns servo to 0 degrees
   robotbit.Servo(servoNumber1, 0)
   basic.clearScreen()
   basic.showString('0')
+
+  // finished turning
   basic.showIcon(IconNames.SmallHeart)
   basic.clearScreen()
   basic.showIcon(IconNames.Heart)
 })
 
+// button B pressed
 input.onButtonPressed(Button.B, function () {
+  // turns servo to 180 degrees
   robotbit.Servo(servoNumber1, 180)
   basic.clearScreen()
   basic.showString('180')
+
+  // finished turning
   basic.showIcon(IconNames.SmallHeart)
   basic.clearScreen()
   basic.showIcon(IconNames.Heart)
