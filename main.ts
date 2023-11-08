34de7709490d5d1261360f1a8e83a9024a326387
Sophie Nguyen <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-11-08 12:40:31 -0500

    git

--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: sophie
+ * Created on: nov 2023
+ * This program moves servo
 */
+
 
 basic.showString('Hello, World!')
