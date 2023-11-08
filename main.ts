e8a57a605b9a0ed6eb4300f6063346a5721c6f62
Sophie Nguyen <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-11-08 12:43:30 -0500

    code

--- main.ts
+++ main.ts
@@ -6,4 +6,26 @@
 */
 
 
-basic.showString('Hello, World!')
+// variables
+const servoNumber1 = robotbit.Servos.S1
+
+// setup
+basic.showIcon(IconNames.Heart)
+
+input.onButtonPressed(Button.A, function () {
+  robotbit.Servo(servoNumber1, 0)
+  basic.clearScreen()
+  basic.showString('0')
+  basic.showIcon(IconNames.SmallHeart)
+  basic.clearScreen()
+  basic.showIcon(IconNames.Heart)
+})
+
+input.onButtonPressed(Button.B, function () {
+  robotbit.Servo(servoNumber1, 180)
+  basic.clearScreen()
+  basic.showString('180')
+  basic.showIcon(IconNames.SmallHeart)
+  basic.clearScreen()
+  basic.showIcon(IconNames.Heart)
+})
