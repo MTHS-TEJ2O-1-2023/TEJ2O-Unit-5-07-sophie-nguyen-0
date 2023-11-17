4d3ec295e1f48dcb500a5c1c3d9cd7b8cc767afc
Sophie Nguyen <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-11-17 18:27:54 +0000

    ewr

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -5,10 +5,36 @@ This module is a Micro:bit MicroPython program makes servo rotate 0 and 180 degr
 """
 
 from microbit import *
+import math
 
-# from servo.py import the Servo class
-from servo import Servo
 
+# Servo 
+class Servo:
+    def __init__(self, pin, freq=50, min_us=600, max_us=2400, angle=180):
+        self.min_us = min_us
+        self.max_us = max_us
+        self.us = 0
+        self.freq = freq
+        self.angle = angle
+        self.analog_period = 0
+        self.pin = pin
+        analog_period = round((1/self.freq) * 1000)  # hertz to miliseconds
+        self.pin.set_analog_period(analog_period)
+
+    def write_us(self, us):
+        us = min(self.max_us, max(self.min_us, us))
+        duty = round(us * 1024 * self.freq // 1000000)
+        self.pin.write_analog(duty)
+        sleep(100)
+        self.pin.write_digital(0)  # turn the pin off
+
+    def write_angle(self, degrees=None):
+        if degrees is None:
+            degrees = math.degrees(radians)
+        degrees = degrees % 360
+        total_range = self.max_us - self.min_us
+        us = self.min_us + total_range * degrees // self.angle
+        self.write_us(us)
 
 # setup
 display.clear()
@@ -18,7 +44,9 @@ sleep(1000)
 while True:
     if button_a.is_pressed():
         # change servo to 0 degrees
-        Servo(pin0).write_angle(0)
+        for angle in range(0, 40, 2):
+            Servo(pin0).write_angle(0)
+
         display.scroll("0")
 
         display.show(Image.HEART)
@@ -26,8 +54,10 @@ while True:
         display.show(Image.HEART_SMALL)
 
     if button_b.is_pressed():
-        # change servo to 0 degrees
-        Servo(pin0).write_angle(180)
+        # change servo to 180 degrees
+        for angle in range(0, 40, 2):
+            Servo(pin0).write_angle(180)
+
         display.scroll("180")
 
         display.show(Image.HEART)
